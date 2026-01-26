7d9d09d3d5553f455441cef28c6da0fcccb0ed4a
KorsanPanda <162370697+KorsanPanda@users.noreply.github.com>
2026-01-26 21:13:38 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.txtPlaka = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnKontrol.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnKontrol.Location = new System.Drawing.Point(149, 259);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(91, 37);
+            this.btnKontrol.TabIndex = 0;
+            this.btnKontrol.Text = "Doğrula";
+            this.btnKontrol.UseVisualStyleBackColor = false;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // txtPlaka
+            // 
+            this.txtPlaka.BackColor = System.Drawing.Color.Silver;
+            this.txtPlaka.Location = new System.Drawing.Point(149, 199);
+            this.txtPlaka.Name = "txtPlaka";
+            this.txtPlaka.Size = new System.Drawing.Size(91, 22);
+            this.txtPlaka.TabIndex = 1;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblSonuc.Location = new System.Drawing.Point(131, 143);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(16, 16);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.Text = "...";
+            this.lblSonuc.Click += new System.EventHandler(this.lblSonuc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(391, 376);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtPlaka);
+            this.Controls.Add(this.btnKontrol);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKontrol;
+        private System.Windows.Forms.TextBox txtPlaka;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            // Eğer "txtPlaka adı mevcut değil" hatası alırsan;
+            // Tasarım ekranında TextBox'ın (Name) kısmını kontrol et.
+            string girilenPlaka = txtPlaka.Text.ToUpper();
+
+            bool sonuc = PlakaDFA.Dogrula(girilenPlaka);
+
+            if (sonuc)
+            {
+                lblSonuc.Text = "GEÇERLİ PLAKA";
+                lblSonuc.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblSonuc.Text = "GEÇERSİZ PLAKA";
+                lblSonuc.ForeColor = Color.Red;
+            }
+        }
+
+        private void lblSonuc_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    // --- DFA MOTORU (Form1 sınıfının dışında ama namespace içinde) ---
+    public static class PlakaDFA
+    {
+        enum State
+        {
+            q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, qHata
+        }
+
+        public static bool Dogrula(string input)
+        {
+            State currentState = State.q0;
+
+            foreach (char c in input)
+            {
+                bool isDigit = Char.IsDigit(c);
+                bool isLetter = Char.IsLetter(c);
+                bool isSpace = (c == ' ');
+
+                switch (currentState)
+                {
+                    case State.q0: // Başlangıç -> Rakam
+                        if (isDigit) currentState = State.q1;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q1: // Rakam -> Rakam
+                        if (isDigit) currentState = State.q2;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q2: // Rakam -> Boşluk (Şehir bitti)
+                        if (isSpace) currentState = State.q3;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q3: // Boşluk -> Harf
+                        if (isLetter) currentState = State.q4;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q4: // Harf -> Harf veya Boşluk
+                        if (isLetter) currentState = State.q5;
+                        else if (isSpace) currentState = State.q7;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q5: // Harf -> Harf veya Boşluk
+                        if (isLetter) currentState = State.q6;
+                        else if (isSpace) currentState = State.q7;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q6: // Harf -> Boşluk (Max 3 harf bitti)
+                        if (isSpace) currentState = State.q7;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q7: // Boşluk -> Rakam (Son kısım başlıyor)
+                        if (isDigit) currentState = State.q8;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q8: // Rakam -> Rakam (Kabul)
+                        if (isDigit) currentState = State.q9;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q9: // Rakam -> Rakam (Kabul)
+                        if (isDigit) currentState = State.q10;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q10: // Rakam -> Rakam (Kabul)
+                        if (isDigit) currentState = State.q11;
+                        else currentState = State.qHata;
+                        break;
+
+                    case State.q11: // Rakam (Son) -> Hata (Fazla karakter)
+                        currentState = State.qHata;
+                        break;
+
+                    case State.qHata:
+                        return false;
+                }
+            }
+
+            // Döngü bittiğinde kabul durumunda mıyız?
+            if (currentState == State.q8 ||
+                currentState == State.q9 ||
+                currentState == State.q10 ||
+                currentState == State.q11)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
